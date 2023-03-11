bacfcf765f2f785eff68971606cefe969e2c8aba
MiquelToni <miqutoni@gmail.com>
2023-03-11 20:42:06 +0100

    add Command model

--- backend/src/jvmMain/kotlin/com/mnm/plugins/Sockets.kt
+++ backend/src/jvmMain/kotlin/com/mnm/plugins/Sockets.kt
@@ -1,5 +1,6 @@
 package com.mnm.plugins
 
+import com.mnm.common.models.Command
 import com.mnm.common.models.PlayerState
 import com.mnm.common.models.PlayingState
 import com.mnm.common.models.Routes
@@ -16,6 +17,9 @@ import kotlinx.coroutines.delay
 
 var playerState = PlayerState(emptyList(), null, PlayingState.STOP, 0)
 
+val commandStop = com.mnm.common.models.Command.Stop()
+val commandPlay = com.mnm.common.models.Command.Play(payload = com.mnm.common.models.PlayPayload(at = 15))
+
 fun Application.configureSockets() {
     install(WebSockets) {
         pingPeriod = Duration.ofSeconds(15)
@@ -29,8 +33,9 @@ fun Application.configureSockets() {
             launch {
                 while(true) {
                     delay(1000)
-                    sendSerialized(playerState)
-                    println(playerState)
+                    sendSerialized(commandStop)
+                    delay(1000)
+                    sendSerialized(commandPlay)
                 }
             }
             for (frame in incoming) {
