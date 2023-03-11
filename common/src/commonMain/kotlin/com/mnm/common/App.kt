083ae03ec13a96938574d30d50549cb9a86cf71c
Miquel Àngel <miquelrc95@outlook.es>
2023-03-11 20:30:01 +0100

    uses a column and fixes serialization for sockets

--- common/src/commonMain/kotlin/com/mnm/common/App.kt
+++ common/src/commonMain/kotlin/com/mnm/common/App.kt
@@ -1,8 +1,10 @@
 package com.mnm.common
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.material.Button
 import androidx.compose.runtime.*
+import com.mnm.common.models.PlayerState
 import com.mnm.common.models.PlayingState
 import com.mnm.common.models.Routes
 import com.mnm.common.networking.client
@@ -10,30 +12,41 @@ import io.ktor.client.plugins.websocket.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.websocket.*
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 
+const val HOST = "827d-85-58-29-37.eu.ngrok.io"
+const val HTTP = "https://$HOST"
+const val WS = "wss://$HOST"
 
 @Composable
 fun App() {
     var text by remember { mutableStateOf("Hello, World!") }
+    var playerState by remember { mutableStateOf<PlayerState?>(null) }
+
     val platformName = getPlatformName()
     val stateFlow = remember { MutableStateFlow(PlayingState.PLAYING) }
 
     LaunchedEffect("") {
-        client.webSocket("wss://b0e8-85-58-29-37.eu.ngrok.io${Routes.api.playlist}") {
+        client.webSocket("$WS${Routes.api.playlist}") {
             launch { stateFlow.collectLatest { sendSerialized(it) } }
-            for (frame in incoming) {
-                text = String(frame.readBytes())
+            while(true) {
+                playerState = receiveDeserialized<PlayerState>()
             }
         }
     }
 
-    Button(onClick = {
-        stateFlow.value = if(stateFlow.value == PlayingState.PLAYING) PlayingState.STOP else PlayingState.PLAYING
-    }) {
-        Text(text)
+    Column {
+
+        Text(playerState.toString())
+        Button(onClick = {
+            stateFlow.value = if(stateFlow.value == PlayingState.PLAYING) PlayingState.STOP else PlayingState.PLAYING
+        }) {
+            Text(text)
+        }
     }
 }
--- common/src/commonMain/kotlin/com/mnm/common/networking/Http.kt
+++ common/src/commonMain/kotlin/com/mnm/common/networking/Http.kt
@@ -3,10 +3,13 @@ package com.mnm.common.networking
 import io.ktor.client.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.websocket.*
+import io.ktor.serialization.kotlinx.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
 
 val client = HttpClient {
-    install(WebSockets)
+    install(WebSockets) {
+        this.contentConverter = KotlinxWebsocketSerializationConverter(Json)
+    }
     install(ContentNegotiation) { json() }
 }
