93919d96d527faa7e556de2ee8db02d679bb4a73
Miquel Àngel <miquelrc95@outlook.es>
2023-03-19 23:37:40 +0100

    Make state processing a bit more idiomatic

--- backend/src/jvmMain/kotlin/com/mnm/plugins/Sockets.kt
+++ backend/src/jvmMain/kotlin/com/mnm/plugins/Sockets.kt
@@ -6,9 +6,9 @@ import io.ktor.serialization.kotlinx.*
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
-import io.ktor.websocket.*
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import java.io.File
 import java.time.Duration
@@ -31,6 +31,48 @@ fun getMusicLibrary() = File(musicPath).walk().filter { it.isFile && it.extensio
     )
 }.toList()
 
+
+fun PlayerState.computeNewState(command: Command) =
+    when (command) {
+        is PrepareSong -> copy(
+            state = if (command.autoplay) PlayingState.PLAYING else PlayingState.READY,
+            playingSongAt = command.songAtPlaylistIndex,
+            currentTime = command.initialSeek
+        )
+
+        is Play ->
+            if (state in listOf(PlayingState.PAUSED, PlayingState.READY))
+                copy(state = PlayingState.PLAYING)
+            else
+                null
+
+        is Pause ->
+            if (state == PlayingState.PLAYING)
+                copy(state = PlayingState.PAUSED, currentTime = command.currentTime)
+            else
+                null
+
+        is SeekTo ->
+            if (state != PlayingState.IDLE)
+                copy(currentTime = command.currentTime, emittedAt = System.currentTimeMillis())
+            else
+                null
+
+        is Stop -> idlePlayerState
+        is Next ->
+            playingSongAt
+                ?.let { currentSong -> (currentSong + 1) % playlist.size }
+                ?.let { copy(playingSongAt = it) }
+
+        is Previous ->
+            playingSongAt
+                ?.let { currentSong -> (currentSong - 1 + playlist.size) % playlist.size }
+                ?.let { copy(playingSongAt = it) }
+    }
+    ?.run { copy(emittedAt = System.currentTimeMillis()) }
+    ?:this
+
+
 fun Application.configureSockets() {
     install(WebSockets) {
         pingPeriod = Duration.ofSeconds(15)
@@ -41,66 +83,16 @@ fun Application.configureSockets() {
     }
     routing {
         webSocket(Routes.api.playlist) {
-            sendSerialized(playerStateFlow.value)
-
-            launch { playerStateFlow.collectLatest { sendSerialized(it) } }
-
-            while (true) {
+            launch {
+                playerStateFlow.collectLatest(::sendSerialized)
+            }
+            while (isActive && incoming.isClosedForReceive.not()) {
                 try {
-                    val newPlayerState: PlayerState? = when (val command = receiveDeserialized<Command>()) {
-                        is PrepareSong -> playerStateFlow.value.copy(
-                            state = if (command.autoplay) PlayingState.PLAYING else PlayingState.READY,
-                            playingSongAt = command.songAtPlaylistIndex,
-                            currentTime = command.initialSeek,
-                            emittedAt = System.currentTimeMillis()
-                        )
-
-                        is Play -> playerStateFlow.value.let {
-                            if (it.state in listOf(PlayingState.PAUSED, PlayingState.READY)) it.copy(
-                                state = PlayingState.PLAYING, emittedAt = System.currentTimeMillis()
-                            )
-                            else null
-                        }
-
-                        is Pause -> playerStateFlow.value.let {
-                            if (it.state == PlayingState.PLAYING) it.copy(
-                                state = PlayingState.PAUSED,
-                                currentTime = command.currentTime,
-                                emittedAt = System.currentTimeMillis()
-                            )
-                            else null
-                        }
-
-                        is SeekTo -> playerStateFlow.value.let {
-                            if (it.state != PlayingState.IDLE) it.copy(
-                                currentTime = command.currentTime, emittedAt = System.currentTimeMillis()
-                            )
-                            else null
-                        }
-
-                        is Stop -> idlePlayerState
-
-                        is Next -> playerStateFlow.value.playingSongAt?.let {
-                            playerStateFlow.value.copy(
-                                playingSongAt = (it + 1) % playerStateFlow.value.playlist.count(),
-                                emittedAt = System.currentTimeMillis()
-                            )
-                        }
-
-                        is Previous -> playerStateFlow.value.playingSongAt?.let {
-                            println(it)
-                            playerStateFlow.value.copy(
-                                playingSongAt = (it + playerStateFlow.value.playlist.count() - 1) % playerStateFlow.value.playlist.count(),
-                                emittedAt = System.currentTimeMillis()
-                            )
-                        }
-                    }
-                    newPlayerState?.let {
-                        playerStateFlow.value = newPlayerState
-                    }
+                    val command = receiveDeserialized<Command>().also { println("received command $it") }
+                    playerStateFlow.value = playerStateFlow.value.computeNewState(command)
                 } catch (e: Exception) {
+                    println(e)
                     println("WebSocket error: '${e.message}'.")
-                    send("Bad Request. Not a Command.")
                 }
             }
         }
