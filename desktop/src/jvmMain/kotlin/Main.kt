9a7a863c839cb5f036e5b1d9f587b04481d1b319
Miquel Àngel <miquelrc95@outlook.es>
2023-03-19 20:31:42 +0100

    Provide window in Desktop app

--- desktop/src/jvmMain/kotlin/Main.kt
+++ desktop/src/jvmMain/kotlin/Main.kt
@@ -1,10 +1,15 @@
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.mnm.common.App
-
+import com.mnm.common.components.ProvideWindow
+import javax.swing.UIManager
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication) {
-        App()
+    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
+    Window(
+        title = "Distributed music player",
+        alwaysOnTop = true,
+        onCloseRequest = ::exitApplication) {
+        ProvideWindow { App() }
     }
 }
