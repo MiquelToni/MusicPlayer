f60d83d752c538ee6975ee8b4507b3ff1a34bf67
Miquel Àngel <miquelrc95@outlook.es>
2023-03-19 20:31:25 +0100

    Adds endpoints

--- common/src/commonMain/kotlin/com/mnm/common/networking/Http.kt
+++ common/src/commonMain/kotlin/com/mnm/common/networking/Http.kt
@@ -1,16 +1,21 @@
 package com.mnm.common.networking
 
-import com.mnm.common.models.*
 import com.mnm.common.HOSTNAME
 import com.mnm.common.HTTP
 import com.mnm.common.PORT
 import com.mnm.common.WS
+import com.mnm.common.models.*
 import io.ktor.client.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.plugins.websocket.*
 import io.ktor.client.request.*
+import io.ktor.client.request.forms.*
+import io.ktor.http.*
 import io.ktor.serialization.kotlinx.*
 import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.polymorphic
@@ -45,6 +50,37 @@ object Http {
     ) {
         val url = getEndpoint(endpoint, schema = WS)
         client.webSocket(urlString = url, request = request, block = block)
+    }
+
+    fun subscribeToPlayerState(scope: CoroutineScope): MutableStateFlow<PlayerState?> {
+        val flow = MutableStateFlow<PlayerState?>(null)
+        scope.launch {
+            webSocket(Routes.api.playlist) {
+                flow.value = receiveDeserialized<PlayerState>()
+            }
+        }
+        return flow
+    }
+
+    suspend fun post(
+        endpoint: String,
+        block: HttpRequestBuilder.() -> Unit = {}
+    ) = client.post(urlString = getEndpoint(endpoint, schema = HTTP), block = block)
+
+    suspend fun uploadFile(
+        fileName: String,
+        fileContents: ByteArray,
+        block: HttpRequestBuilder.() -> Unit = {}
+    ) {
+        client.submitFormWithBinaryData(
+            url = getEndpoint(Routes.api.postNewSong, schema=HTTP),
+            formData = formData {
+                append("file", fileContents, headers = Headers.build {
+                    append(HttpHeaders.ContentDisposition, "filename=$fileName")
+                })
+            },
+            block = block
+        )
     }
 }
 
